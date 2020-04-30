920a0dab516abf3535d59e022581f89e6f7c117f
Dmitry Chervonenko <dima1772000@gmail.com>
2020-04-30 20:09:56 +0300

    delete admin panel

--- src/components/admin-panel/admin-panel.jsx
+++ src/components/admin-panel/admin-panel.jsx
@@ -1,11 +1,7 @@
 import React, {Component} from "react";
 import './admin-panel.css';
-import RoomsService from "../../services/rooms-service";
-import Spinner from "../spinner/spinner";
-import AdminRoom from "../admin-room/admin-room";
 
 class AdminPanel extends Component {
-    roomsService = new RoomsService();
 
     state = {
         loading: true,
@@ -13,50 +9,14 @@ class AdminPanel extends Component {
     };
 
     componentDidMount() {
-        this.fetchAdminRooms();
+
     }
 
-    #onAdminRoomsLoaded = (rooms) => {
-        this.setState({
-            loading: false,
-            roomsList: rooms,
-        });
-    };
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        const { loading } = this.state;
-        return !(nextState.loading === loading && loading === false);
-    }
-
-    fetchAdminRooms = () => {
-        this.roomsService.getAllRooms()
-            .then((rooms) => {
-                this.#onAdminRoomsLoaded(rooms);
-            });
-    };
 
 
     render() {
-        const { loading, roomsList } = this.state;
-
-        if (loading) {
-            return (
-                <Spinner />
-            );
-        } else {
-            const roomsModified = roomsList.map((room) => {
-                return <li>
-                    <AdminRoom
-                        room = {room}
-                        accessToken = {localStorage.getItem('access_token')}
-                    />
-                </li>
-            });
-            return <ul>
-                {roomsModified}
-            </ul>;
-        }
-
+        return null;
     }
 }
 
